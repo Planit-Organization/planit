0606a9cd8c1eae25867cc54e2d4a909b3fc30176
Alura <aluracm@MacBook-Pro.local>
2021-06-24 15:24:21 -0500

    more merge conflicts

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -5,46 +5,28 @@ import DashBoardContainer from "./DashBoardContainer";
 import LoginPage from "../components/LoginPage";
 import RegisterPage from "../components/RegisterPage";
 
-<<<<<<< HEAD
-const MainContainer = props => {
-
-
-  return (<div>
-    {/* <Navbar></Navbar> */}
-     <Switch>
-=======
 const MainContainer = (props) => {
   return (
     <div>
       <Navbar></Navbar>
       <Switch>
->>>>>>> dev
         <Route
           exact path = "/"
           // component = {LandingPageContainer} //welcome page with Navbar (sign up/login)
         />
         <Route
-<<<<<<< HEAD
-          path = "/calendar" // /calendar/date -> todolsit
-          // component = {DashboardContainer}
-=======
           path="/calendar" // /calendar/date -> todolsit
           component={DashBoardContainer}
->>>>>>> dev
         />
 
 
         <Route
           exact path = "/login"
-<<<<<<< HEAD
-          // component = {LoginContainer}
-=======
           component = {LoginPage}
         />
         <Route
           exact path = "/register"
           component = {RegisterPage}
->>>>>>> dev
         />
         <Route
           exact path = "/signup"
