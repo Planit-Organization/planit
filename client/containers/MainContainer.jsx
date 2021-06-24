767ac4e2cd968063a1fc34fc0f6782946fa12032
jungholee27 <81586253+jungholee27@users.noreply.github.com>
2021-06-24 15:27:19 -0400

    Update MainContainer.jsx

--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Navbar from "./NavBar";
+import Navbar from "./Navbar";
 import DashBoardContainer from "./DashBoardContainer";
 import LoginPage from "../components/LoginPage";
 import RegisterPage from "../components/RegisterPage";
